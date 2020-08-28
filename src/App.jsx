4793eaffad0cf898594417a26783badedc3462e0
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-08-28 14:12:01 +0530

    fix: indentation

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import 'h8k-components';
 import Slides from './components/Slides';
 const title = "Slideshow App";
 
-function App({slides}) {
-    return (
-        <div>
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="App">
-                <Slides slides={slides} />
-            </div>
-        </div>
-    );
+function App({ slides }) {
+  return (
+    <div>
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="App">
+        <Slides slides={slides} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
